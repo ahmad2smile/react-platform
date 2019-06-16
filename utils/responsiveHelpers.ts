1e96a5c1816c4cd8e865730a88b3e616c478160b
Ahmad <ahmad2smile@protonmail.com>
2019-06-17 00:55:19 +0700

    Updated responsive helpers

--- utils/responsiveHelpers.ts
+++ utils/responsiveHelpers.ts
@@ -1,22 +1,38 @@
 import { Styles } from "react-jss"
 
-export function mobileUpTo720 (styles: Styles): Styles {
+export function breakPointXSmall(styles: Styles): Styles {
 	return {
-		"@media (max-width: 720px)": {
+		"@media (max-width: 420px)": {
 			...styles
 		}
 	}
 }
 
-export function mobileUpTo1024 (styles: Styles): Styles {
+export function breakPointSmall(styles: Styles): Styles {
 	return {
-		"@media (max-width: 1024px)": {
+		"@media (max-width: 560px)": {
 			...styles
 		}
 	}
 }
 
-export function mobileUpTo1200 (styles: Styles): Styles {
+export function breakPointMedium(styles: Styles): Styles {
+	return {
+		"@media (max-width: 800px)": {
+			...styles
+		}
+	}
+}
+
+export function breakPointLarge(styles: Styles): Styles {
+	return {
+		"@media (max-width: 1000px)": {
+			...styles
+		}
+	}
+}
+
+export function breakPointXLarge(styles: Styles): Styles {
 	return {
 		"@media (max-width: 1200px)": {
 			...styles
